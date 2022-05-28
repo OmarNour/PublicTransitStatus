ed7e3d7d1023587f2ed03b22deab2e10e2ccf091
Omar Nour <omar_nour@hotmail.com>
2022-05-28 13:17:09 +0200

    produce

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -12,6 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 class Turnstile(Producer):
+    topic_name = "org.chicago.cta.turnstiles.table.v1"
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
     value_schema = avro.load(
        f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
@@ -20,7 +21,7 @@ class Turnstile(Producer):
     def __init__(self, station):
         """Create the Turnstile"""
         super().__init__(
-            topic_name=f"tpc_turnstile_{station.station_name}",
+            topic_name=Turnstile.topic_name,
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
             num_partitions=1,
@@ -32,14 +33,17 @@ class Turnstile(Producer):
     def run(self, timestamp, time_step):
         """Simulates riders entering through the turnstile."""
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
-        logger.info("turnstile kafka integration incomplete - skipping")
-        for _ in range(num_entries):
-            self.producer.produce(
-                topic=self.topic_name,
-                key={"timestamp": self.time_millis()},
-                value={
-                    "station_id": self.station.station_id,
-                    "station_name": self.station.station_name,
-                    "line": self.station.color.name
-                },
-            )
+        # logger.info("turnstile kafka integration incomplete - skipping")
+        try:
+            for _ in range(num_entries):
+                self.producer.produce(
+                    topic=self.topic_name,
+                    key={"timestamp": self.time_millis()},
+                    value={
+                        "station_id": self.station.station_id,
+                        "station_name": self.station.station_name,
+                        "line": self.station.color.name
+                    },
+                )
+        except Exception as e:
+            logger.info(f"failed to producer data for topic {self.topic_name}: {e}")
